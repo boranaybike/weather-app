0cf1d98932514a887b08661d6b6720532a47c973
ik3 <aybikeboran7140@gmail.com>
2022-04-23 16:56:54 +0300

    add optional date

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, OnInit, EventEmitter, Input } from '@angular/core';
 import { IWeather } from '../../models/response/weather.response';
 import { WeatherService } from '../../services/weather.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,20 +13,30 @@ export class HomeComponent implements OnInit {
   mood: string;
   weathers: IWeather = <IWeather>{};
   today = new Date();
+  today_string: string;
+  date: string;
+
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
     this.getWeather();
+    this.today_string = this.today.toDateString();
   }
   getWeather() {
     let hour = this.today.getHours();
     this.weatherService.getWeather().subscribe((data) => {
       this.weathers = data;
-      this.temperature = this.weathers.hourly.temperature_2m[hour - 1];
+      this.temperature = this.weathers.hourly.temperature_2m[hour];
       this.getMood();
     });
   }
+  changeTemp() {
+    let i = this.weathers.hourly.time.indexOf(this.date);
+    this.temperature = this.weathers.hourly.temperature_2m[i];
+    this.today_string = this.date;
 
+    this.getMood();
+  }
   getMood() {
     let moods: string[] = [
       'snowy', //0-5
@@ -34,7 +45,6 @@ export class HomeComponent implements OnInit {
       'cloudy', //15-20
       'partly-cloudy', //20-25
       'sunny', //25-30
-      'sunny', //30-35
     ];
     this.mood = moods[Math.floor(this.temperature / 5)];
   }
